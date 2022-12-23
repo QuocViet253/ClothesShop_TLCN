9dc92f854e4bf5d66e3c6abc72e8bc3a78ef7da1
Jeremie <tranvolam12345@gmail.com>
2022-12-23 13:26:03 +0700

    update

--- ClothesShop_Client/src/app/login/login.component.ts
+++ ClothesShop_Client/src/app/login/login.component.ts
@@ -59,6 +59,7 @@ export class LoginComponent implements OnInit {
           this.appService.login();
           this.toastr.success('Đăng nhập thành công !');
           setTimeout(window.location.reload.bind(window.location), 250);
+          this.router.navigate(['']);
         } else {
           this.toastr.warning('Thông tin đăng nhập không chính xác !');
           localStorage.removeItem('UserInfo');
